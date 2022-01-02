224e3cf8f481a8735726438f9b0947c63dbbada8
starNGC2237 <starmars_lu@163.com>
2022-01-02 18:54:13 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
+/*
+ * @Author: zhilutianji
+ * @Date: 2022-01-02 18:49:33
+ * @LastEditors: zhilutianji
+ * @LastEditTime: 2022-01-02 18:53:34
+ * @Description: file content
+ * @FilePath: \shops_frontend_web\src\main.js
+ */
 import Vue from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 
 Vue.config.productionTip = false
 
