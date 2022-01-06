78b328401689afa05915223b1b984b94ebd93d68
starNGC2237 <starmars_lu@163.com>
2022-01-07 00:53:47 +0800

    fix:banner

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -2,14 +2,14 @@
  * @Author: zhilutianji
  * @Date: 2022-01-02 19:31:11
  * @LastEditors: zhilutianji
- * @LastEditTime: 2022-01-07 00:36:15
+ * @LastEditTime: 2022-01-07 00:53:06
  * @Description: file content
  * @FilePath: \shops_frontend_web\src\pages\Home\index.vue
 -->
 <template>
   <div>
     <h1>Home</h1>
-    <LongBar></LongBar>
+    <LongBanner></LongBanner>
     <BrickBox></BrickBox>
     <Footer></Footer>
   </div>
@@ -17,13 +17,13 @@
 
 <script>
 import Footer from "@/components/Footer";
-import LongBar from "@/pages/Home/LongBar/index.vue"
+import LongBanner from "@/pages/Home/LongBanner/index.vue"
 import BrickBox from "@/pages/Home/BrickBox/index.vue"
 export default {
   name: "Home",
   components: {
     Footer,
-    LongBar,
+    LongBanner,
     BrickBox
   },
 };
