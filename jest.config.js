c0ef782ad43ee56b532818ff4cea062ae6b1f472
starNGC2237 <starmars_lu@163.com>
2022-04-02 16:50:43 +0800

    change

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
         '\\.(css|less)$': 'identity-obj-proxy'
-    }
+    },
+    collectCoverage: true,
+    // 覆盖文件范围（目前前端单元测试不关注覆盖率，所以这里暂时为空）
+    collectCoverageFrom: []
 }
